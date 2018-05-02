7d858255ad8df22bc09a7e4089f4dd21f6e876fc
Joel Falcou <joel.falcou@lri.fr>
2018-05-02 12:11:54 +0200

    Basic infrastructure files

---
+++ README.md
@@ -0,0 +1,16 @@
+# SPY - C++17 preprocessor-less informations gathering
+
+## Purpose
+Detection and versioning of operating systems, compilers, architecture and other element are
+traditionally done using preprocessor macros. Library like Boost.Predef [link] provides a sanitized
+interface on top of those but still live in a world where the preprocessor is king.
+
+SPY is a C++17 (and onward) library that gathers similar informations and provides a constexpr
+compatible interface to access those information, thus making their exploitation safe within the
+now widely accepted if constexpr language construct.
+
+## Basic usage
+
+## Support
+
+## Useful Links
---
+++ cmake/test.cmake
@@ -0,0 +1,31 @@
+## -------------------------------------------------------------------------------------------------
+##                              Copyright 2018 Joel FALCOU
+##
+##                   Distributed under the Boost Software License, Version 1.0.
+##                        See accompanying file LICENSE.txt or copy at
+##                            http://www.boost.org/LICENSE_1_0.txt
+## -------------------------------------------------------------------------------------------------
+
+include(download)
+
+## Disable testing LEST
+set(LEST_BUILD_TEST OFF CACHE INTERNAL    "OFF")
+set(LEST_BUILD_EXAMPLE OFF CACHE INTERNAL "OFF")
+
+download_project( PROJ                lest
+                  GIT_REPOSITORY      https://github.com/martinmoene/lest
+                  GIT_TAG             master
+                  "UPDATE_DISCONNECTED 1"
+                  QUIET
+                )
+
+add_subdirectory(${lest_SOURCE_DIR} ${lest_BINARY_DIR})
+include_directories("${lest_SOURCE_DIR}/include")
+include_directories("${PROJECT_SOURCE_DIR}/test")
+
+## Setup our tests
+add_custom_target(tests)
+add_custom_target(unit)
+add_dependencies(tests unit)
+
+#add_subdirectory(${PROJECT_SOURCE_DIR}/test/)
