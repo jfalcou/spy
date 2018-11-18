18742a87b140c2d60992cf0cf4a26f406e7330f6
Joel Falcou <joel.falcou@lri.fr>
2018-11-18 18:25:35 +0100

    Licencing: Boost -> MIT

--- cmake/download.cmake
+++ cmake/download.cmake
@@ -1,9 +1,8 @@
 ## -------------------------------------------------------------------------------------------------
-##                     From https://github.com/Crascit/DownloadProject
+##  From https://github.com/Crascit/DownloadProject
 ##
-##                   Distributed under the Boost Software License, Version 1.0.
-##                        See accompanying file LICENSE.txt or copy at
-##                            http://www.boost.org/LICENSE_1_0.txt
+##  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+##  SPDX-License-Identifier: MIT
 ## -------------------------------------------------------------------------------------------------
 set(_DownloadProjectDir "${CMAKE_CURRENT_LIST_DIR}")
 
--- include/spy/compiler.hpp
+++ include/spy/compiler.hpp
@@ -2,8 +2,8 @@
 /*
   Copyright 2018 Joel FALCOU
 
-  Distributed under the Boost Software License, Version 1.0.
-  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+  Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+  SPDX-License-Identifier: MIT
  */
 //==================================================================================================
 #ifndef SPY_COMPILER_HPP_INLUDED
