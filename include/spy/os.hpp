377ab51d902faa5fb1a1dc28855cf0e48fed9db6
Joel Falcou <joel.falcou@lri.fr>
2019-11-17 21:20:24 +0100

    Update (c) dates

--- include/spy/os.hpp
+++ include/spy/os.hpp
@@ -1,6 +1,6 @@
 //==================================================================================================
 /*
-  Copyright 2018 Joel FALCOU
+  Copyright 2018-2019 Joel FALCOU
 
   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
   SPDX-License-Identifier: MIT
