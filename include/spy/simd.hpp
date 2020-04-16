47a19e733b56c547da49988916717ff4f12bee56
Joel Falcou <joel.falcou@lri.fr>
2020-04-16 19:54:43 +0200

    Add support and docs for AVX512 detection
    AVX512 comes with a gazillions supplemental IS so they're
    all detected and accessible via spy::supports::avx512
    
    * Updated SIMD test
    * Added docs

--- include/spy/simd.hpp
+++ include/spy/simd.hpp
@@ -20,10 +20,11 @@ namespace spy::detail
   enum class simd_isa { undefined_ = -1, x86_ = 1000, ppc_ = 2000, arm_ = 3000 };
 
   enum class simd_version { undefined_ = -1
-                          , sse1_  = 1110, sse2_  = 1120, sse3_ = 1130, ssse3_ = 1131
-                          , sse41_ = 1141, sse42_ = 1142, avx_  = 1201, avx2_  = 1202
-                          , vmx_   = 2001, vsx_   = 2002
-                          , neon_  = 3001
+                          , sse1_   = 1110, sse2_  = 1120, sse3_ = 1130, ssse3_ = 1131
+                          , sse41_  = 1141, sse42_ = 1142, avx_  = 1201, avx2_  = 1202
+                          , avx512_ = 1300
+                          , vmx_    = 2001, vsx_   = 2002
+                          , neon_   = 3001
                           };
 
   template<simd_isa ISA = simd_isa::undefined_, simd_version VERSION = simd_version::undefined_>
@@ -34,17 +35,18 @@ namespace spy::detail
 
     friend std::ostream& operator<<(std::ostream& os, simd_info const&)
     {
-            if constexpr ( VERSION == simd_version::sse1_ ) os << "X86 SSE";
-      else  if constexpr ( VERSION == simd_version::sse2_ ) os << "X86 SSE2";
-      else  if constexpr ( VERSION == simd_version::sse3_ ) os << "X86 SSE3";
-      else  if constexpr ( VERSION == simd_version::ssse3_) os << "X86 SSSE3";
-      else  if constexpr ( VERSION == simd_version::sse41_) os << "X86 SSE4.1";
-      else  if constexpr ( VERSION == simd_version::sse42_) os << "X86 SSE4.2";
-      else  if constexpr ( VERSION == simd_version::avx_  ) os << "X86 AVX";
-      else  if constexpr ( VERSION == simd_version::avx2_ ) os << "X86 AVX2";
-      else  if constexpr ( VERSION == simd_version::vmx_  ) os << "PPC VMX";
-      else  if constexpr ( VERSION == simd_version::vsx_  ) os << "PPC VSX";
-      else  if constexpr ( VERSION == simd_version::neon_ ) os << "ARM NEON";
+            if constexpr ( VERSION == simd_version::sse1_   ) os << "X86 SSE";
+      else  if constexpr ( VERSION == simd_version::sse2_   ) os << "X86 SSE2";
+      else  if constexpr ( VERSION == simd_version::sse3_   ) os << "X86 SSE3";
+      else  if constexpr ( VERSION == simd_version::ssse3_  ) os << "X86 SSSE3";
+      else  if constexpr ( VERSION == simd_version::sse41_  ) os << "X86 SSE4.1";
+      else  if constexpr ( VERSION == simd_version::sse42_  ) os << "X86 SSE4.2";
+      else  if constexpr ( VERSION == simd_version::avx_    ) os << "X86 AVX";
+      else  if constexpr ( VERSION == simd_version::avx2_   ) os << "X86 AVX2";
+      else  if constexpr ( VERSION == simd_version::avx512_ ) os << "X86 AVX512";
+      else  if constexpr ( VERSION == simd_version::vmx_    ) os << "PPC VMX";
+      else  if constexpr ( VERSION == simd_version::vsx_    ) os << "PPC VSX";
+      else  if constexpr ( VERSION == simd_version::neon_   ) os << "ARM NEON";
       else return os << "Undefined SIMD instructions set";
 
       if constexpr (spy::supports::aarch64_) os << " (with AARCH64 support)";
@@ -119,14 +121,15 @@ namespace spy
   using x86_simd_info = detail::simd_info<detail::simd_isa::x86_,V>;
 
   constexpr inline auto x86_simd_ = x86_simd_info<>{};
-  constexpr inline auto sse1_     = x86_simd_info<detail::simd_version::sse1_ >{};
-  constexpr inline auto sse2_     = x86_simd_info<detail::simd_version::sse2_ >{};
-  constexpr inline auto sse3_     = x86_simd_info<detail::simd_version::sse3_ >{};
-  constexpr inline auto ssse3_    = x86_simd_info<detail::simd_version::ssse3_>{};
-  constexpr inline auto sse41_    = x86_simd_info<detail::simd_version::sse41_>{};
-  constexpr inline auto sse42_    = x86_simd_info<detail::simd_version::sse42_>{};
-  constexpr inline auto avx_      = x86_simd_info<detail::simd_version::avx_  >{};
-  constexpr inline auto avx2_     = x86_simd_info<detail::simd_version::avx2_ >{};
+  constexpr inline auto sse1_     = x86_simd_info<detail::simd_version::sse1_   >{};
+  constexpr inline auto sse2_     = x86_simd_info<detail::simd_version::sse2_   >{};
+  constexpr inline auto sse3_     = x86_simd_info<detail::simd_version::sse3_   >{};
+  constexpr inline auto ssse3_    = x86_simd_info<detail::simd_version::ssse3_  >{};
+  constexpr inline auto sse41_    = x86_simd_info<detail::simd_version::sse41_  >{};
+  constexpr inline auto sse42_    = x86_simd_info<detail::simd_version::sse42_  >{};
+  constexpr inline auto avx_      = x86_simd_info<detail::simd_version::avx_    >{};
+  constexpr inline auto avx2_     = x86_simd_info<detail::simd_version::avx2_   >{};
+  constexpr inline auto avx512_   = x86_simd_info<detail::simd_version::avx512_ >{};
 
   template<detail::simd_version V = detail::simd_version::undefined_>
   using ppc_simd_info = detail::simd_info<detail::simd_isa::ppc_,V>;
