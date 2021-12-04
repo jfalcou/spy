db36f1316f0c943db3abbe4841cb28e23ff16b42
Joel Falcou <joel.falcou@lri.fr>
2021-12-04 09:16:09 +0100

    Fix gcc path

--- test/toolchain/gcc.ppc64.release.cmake
+++ test/toolchain/gcc.ppc64.release.cmake
@@ -6,9 +6,9 @@
 set(CMAKE_SYSTEM_NAME Linux         )
 set(CMAKE_SYSTEM_PROCESSOR powerpc  )
 
-set(CMAKE_C_COMPILER    powerpc64-linux-gnu-gcc-10  )
-set(CMAKE_CXX_COMPILER  powerpc64-linux-gnu-g++-10  )
-set(CMAKE_BUILD_TYPE    Release                     )
-set(CMAKE_CXX_FLAGS     "-static"                   )
+set(CMAKE_C_COMPILER    powerpc64-linux-gnu-gcc-11 )
+set(CMAKE_CXX_COMPILER  powerpc64-linux-gnu-g++-11 )
+set(CMAKE_BUILD_TYPE    Release                    )
+set(CMAKE_CXX_FLAGS     "-static"                  )
 
 set(CMAKE_CROSSCOMPILING_CMD qemu-ppc64)
