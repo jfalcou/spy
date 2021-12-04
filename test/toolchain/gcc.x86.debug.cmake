fb0eb880ebbd15b43e04067398feca4be805e798
Joel Falcou <joel.falcou@lri.fr>
2021-12-04 09:41:10 +0100

    Update Docker & toolchain for CI

--- test/toolchain/gcc.x86.debug.cmake
+++ test/toolchain/gcc.x86.debug.cmake
@@ -3,6 +3,6 @@
 ##  Copyright : SPY Contributors & Maintainers
 ##  SPDX-License-Identifier: MIT
 ##==================================================================================================
-set(CMAKE_C_COMPILER    gcc-10  )
-set(CMAKE_CXX_COMPILER  g++-10  )
-set(CMAKE_BUILD_TYPE    Debug   )
+set(CMAKE_C_COMPILER    gcc   )
+set(CMAKE_CXX_COMPILER  g++   )
+set(CMAKE_BUILD_TYPE    Debug )
