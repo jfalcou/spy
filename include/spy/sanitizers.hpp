ec069156f3cc7bc6ccf0e9a29c5bb67ac879809d
Joel Falcou <joel.falcou@lri.fr>
2022-04-14 22:05:12 +0200

    Fix #15 - Detect and supprots sanitizers detection/disabling

---
+++ include/spy/sanitizers.hpp
@@ -0,0 +1,62 @@
+//==================================================================================================
+/**
+  SPY - C++ Informations Broker
+  Copyright : SPY Contributors & Maintainers
+  SPDX-License-Identifier: MIT
+**/
+//==================================================================================================
+#pragma once
+#include <spy/compiler.hpp>
+
+// Clang detection
+#if defined(__has_feature)
+#  if __has_feature(address_sanitizer)
+#     define SPY_ADDRESS_SANITIZERS_ENABLED
+#  endif
+#  if __has_feature(thread_sanitizer)
+#     define SPY_THREAD_SANITIZERS_ENABLED
+#  endif
+#endif
+
+// g++/MSVC detection
+#if !defined(SPY_ADDRESS_SANITIZERS_ENABLED)
+#  if defined(__SANITIZE_ADDRESS__)
+#     define SPY_ADDRESS_SANITIZERS_ENABLED
+#  endif
+#endif
+
+#if !defined(SPY_THREAD_SANITIZERS_ENABLED)
+#  if defined(__SANITIZE_THREAD__)
+#     define SPY_THREAD_SANITIZERS_ENABLED
+#  endif
+#endif
+
+namespace spy::supports
+{
+#if defined(SPY_ADDRESS_SANITIZERS_ENABLED)
+  constexpr bool address_sanitizers_status = true;
+#else
+  constexpr bool address_sanitizers_status = false;
+#endif
+
+#if defined(SPY_THREAD_SANITIZERS_ENABLED)
+  constexpr bool thread_sanitizers_status = true;
+#else
+  constexpr bool thread_sanitizers_status = false;
+#endif
+
+  constexpr bool sanitizers_status = address_sanitizers_status || thread_sanitizers_status;
+}
+
+#if defined(SPY_COMPILER_IS_CLANG) || defined(SPY_COMPILER_IS_GCC)
+#define SPY_DISABLE_ADDRESS_SANITIZERS  __attribute__((no_sanitize_address))
+#define SPY_DISABLE_THREAD_SANITIZERS   __attribute__((no_sanitize_thread))
+#elif defined(SPY_COMPILER_IS_MSVC)
+#define SPY_DISABLE_ADDRESS_SANITIZERS  __declspec(no_sanitize_address)
+#define SPY_DISABLE_THREAD_SANITIZERS
+#else
+#define SPY_DISABLE_ADDRESS_SANITIZERS
+#define SPY_DISABLE_THREAD_SANITIZERS
+#endif
+
+#define SPY_DISABLE_SANITIZERS SPY_DISABLE_ADDRESS_SANITIZERS SPY_DISABLE_THREAD_SANITIZERS
