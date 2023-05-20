c9b1c3ac74bae33420b1ba3cb32035807f07ee6a
Joel Falcou <joel.falcou@lri.fr>
2023-05-20 19:24:21 +0200

    Proper MAC OS X toolchain file

---
+++ test/toolchain/clang.x86_osx.debug.cmake
@@ -0,0 +1,8 @@
+##==================================================================================================
+##  SPY - C++ Informations Broker
+##  Copyright : SPY Project Contributors
+##  SPDX-License-Identifier: BSL-1.0
+##==================================================================================================
+set(CMAKE_C_COMPILER    /usr/local/opt/llvm@15/bin/clang    )
+set(CMAKE_CXX_COMPILER  /usr/local/opt/llvm@15/bin/clang++  )
+set(CMAKE_BUILD_TYPE    Debug                               )
