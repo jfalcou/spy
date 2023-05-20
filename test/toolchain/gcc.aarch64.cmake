54b8cad779490e33dbd470dd81390ccf7a0452fa
Joel Falcou <joel.falcou@lri.fr>
2023-05-20 19:51:35 +0200

    Fix compiler versions

--- test/toolchain/gcc.aarch64.cmake
+++ test/toolchain/gcc.aarch64.cmake
@@ -6,7 +6,7 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_C_COMPILER    aarch64-linux-gnu-gcc-11 )
-set(CMAKE_CXX_COMPILER  aarch64-linux-gnu-g++-11 )
+set(CMAKE_C_COMPILER    aarch64-linux-gnu-gcc-12 )
+set(CMAKE_CXX_COMPILER  aarch64-linux-gnu-g++-12 )
 
 set(CMAKE_CROSSCOMPILING_CMD qemu-aarch64)
