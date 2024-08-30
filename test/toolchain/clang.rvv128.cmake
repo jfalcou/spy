66403d579350b584749f251ae3d658aeebd6a24f
Joel Falcou <joel.falcou@lri.fr>
2024-08-30 21:44:24 +0200

    Enable RISC-V CI

---
+++ test/toolchain/clang.rvv128.cmake
@@ -0,0 +1,13 @@
+##==================================================================================================
+##  SPY - C++ Informations Broker
+##  Copyright : SPY Project Contributors
+##  SPDX-License-Identifier: BSL-1.0
+##==================================================================================================
+set(CMAKE_SYSTEM_NAME       Generic)
+set(CMAKE_SYSTEM_PROCESSOR  riscv64)
+
+set(CMAKE_C_COMPILER    /opt/llvm/bin/clang)
+set(CMAKE_CXX_COMPILER  /opt/llvm/bin/clang++)
+
+set(CMAKE_CXX_FLAGS "-O3 -march=rv64gcv -std=c++20 -mrvv-vector-bits=128 --static --target=riscv64-unknown-linux-gnu ${EVE_OPTIONS}" )
+set(CMAKE_CROSSCOMPILING_CMD ${PROJECT_SOURCE_DIR}/test/toolchain/run_rvv128.sh )
