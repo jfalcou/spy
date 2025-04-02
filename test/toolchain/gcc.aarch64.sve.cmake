986c3eef4fbdd8e03cb9907d3394d196e8408876
Joel Falcou <joel.falcou@lri.fr>
2025-04-02 22:19:05 +0200

    Move to CI v9

--- test/toolchain/gcc.aarch64.sve.cmake
+++ test/toolchain/gcc.aarch64.sve.cmake
@@ -6,8 +6,8 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_C_COMPILER    aarch64-linux-gnu-gcc-13 )
-set(CMAKE_CXX_COMPILER  aarch64-linux-gnu-g++-13 )
+set(CMAKE_C_COMPILER    aarch64-linux-gnu-gcc-14 )
+set(CMAKE_CXX_COMPILER  aarch64-linux-gnu-g++-14 )
 set(CMAKE_CXX_FLAGS     "-march=armv8-a+sve -msve-vector-bits=512" )
 
 set(CMAKE_CROSSCOMPILING_CMD qemu-aarch64)
