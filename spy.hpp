5a82f168db9b9b6ed01fee1f4679b3a51aa55c84
SPY Standalone Generator <spy@nowhere.com>
2026-01-17 19:28:17 +0000

    Latest SPY standalone

--- spy.hpp
+++ spy.hpp
@@ -1267,63 +1267,63 @@ namespace spy::literal
   }
 }
 #if defined(SPY_ARCH_IS_ARM)
-    #if defined(__ARM_FP16_FORMAT_IEEE) && __ARM_FP16_FORMAT_IEEE == 1                    \
-    && (!defined(__ARM_FP16_FORMAT_ALTERNATIVE) || __ARM_FP16_FORMAT_ALTERNATIVE == 0)
-        #define SPY_SUPPORTS_FP16_TYPE
-        #if defined(__ARM_FEATURE_FP16_SCALAR_ARITHMETIC)
-            #define SPY_SUPPORTS_FP16_SCALAR_OPS
-        #endif
-        #if defined(__ARM_FEATURE_FP16_VECTOR_ARITHMETIC)
-            #define SPY_SUPPORTS_FP16_VECTOR_OPS
-            #define SPY_SUPPORTS_FP16_VECTOR_CONVERSION
-        #elif defined(__aarch64__)
-            #define SPY_SUPPORTS_FP16_VECTOR_CONVERSION
-        #endif
-    #endif
+#if defined(__ARM_FP16_FORMAT_IEEE) && __ARM_FP16_FORMAT_IEEE == 1 &&                                                  \
+  (!defined(__ARM_FP16_FORMAT_ALTERNATIVE) || __ARM_FP16_FORMAT_ALTERNATIVE == 0)
+#define SPY_SUPPORTS_FP16_TYPE
+#if defined(__ARM_FEATURE_FP16_SCALAR_ARITHMETIC)
+#define SPY_SUPPORTS_FP16_SCALAR_OPS
+#endif
+#if defined(__ARM_FEATURE_FP16_VECTOR_ARITHMETIC)
+#define SPY_SUPPORTS_FP16_VECTOR_OPS
+#define SPY_SUPPORTS_FP16_VECTOR_CONVERSION
+#elif defined(__aarch64__)
+#define SPY_SUPPORTS_FP16_VECTOR_CONVERSION
+#endif
+#endif
 #elif defined(SPY_ARCH_IS_X86)
-    #define SPY_SUPPORTS_FP16_TYPE
+#define SPY_SUPPORTS_FP16_TYPE
 #elif defined(SPY_ARCH_IS_AMD64)
-    #define SPY_SUPPORTS_FP16_TYPE
-    #if defined(__AVX512FP16__)
-        #define SPY_SUPPORTS_FP16_SCALAR_OPS
-        #define SPY_SUPPORTS_FP16_VECTOR_OPS
-        #define SPY_SUPPORTS_FP16_VECTOR_CONVERSION
-    #elif defined(__F16C__)
-        #define SPY_SUPPORTS_FP16_VECTOR_CONVERSION
-    #endif
+#define SPY_SUPPORTS_FP16_TYPE
+#if defined(__AVX512FP16__)
+#define SPY_SUPPORTS_FP16_SCALAR_OPS
+#define SPY_SUPPORTS_FP16_VECTOR_OPS
+#define SPY_SUPPORTS_FP16_VECTOR_CONVERSION
+#elif defined(__F16C__)
+#define SPY_SUPPORTS_FP16_VECTOR_CONVERSION
+#endif
 #elif defined(SPY_ARCH_IS_RISCV)
-    #define SPY_SUPPORTS_FP16_TYPE
-    #if defined(__riscv_zfh)
-        #define SPY_SUPPORTS_FP16_SCALAR_OPS
-    #endif
-    #if defined(__riscv_zvfh)
-        #define SPY_SUPPORTS_FP16_VECTOR_OPS
-        #define SPY_SUPPORTS_FP16_VECTOR_CONVERSION
-    #elif defined(__riscv_zvfhmin)
-        #define SPY_SUPPORTS_FP16_VECTOR_CONVERSION
-    #endif
+#define SPY_SUPPORTS_FP16_TYPE
+#if defined(__riscv_zfh)
+#define SPY_SUPPORTS_FP16_SCALAR_OPS
+#endif
+#if defined(__riscv_zvfh)
+#define SPY_SUPPORTS_FP16_VECTOR_OPS
+#define SPY_SUPPORTS_FP16_VECTOR_CONVERSION
+#elif defined(__riscv_zvfhmin)
+#define SPY_SUPPORTS_FP16_VECTOR_CONVERSION
+#endif
 #endif
 namespace spy::supports::fp16
 {
 #ifdef SPY_SUPPORTS_FP16_TYPE
-    static constexpr bool type = true;
+  static constexpr bool type = true;
 #else
-    static constexpr bool type = false;
+  static constexpr bool type = false;
 #endif
 #ifdef SPY_SUPPORTS_FP16_SCALAR_OPS
-    static constexpr bool scalar_ops = true;
+  static constexpr bool scalar_ops = true;
 #else
-    static constexpr bool scalar_ops = false;
+  static constexpr bool scalar_ops = false;
 #endif
 #ifdef SPY_SUPPORTS_FP16_VECTOR_CONVERSION
-    static constexpr bool vector_conversion = true;
+  static constexpr bool vector_conversion = true;
 #else
-    static constexpr bool vector_conversion = false;
+  static constexpr bool vector_conversion = false;
 #endif
 #ifdef SPY_SUPPORTS_FP16_VECTOR_OPS
-    static constexpr bool vector_ops = true;
+  static constexpr bool vector_ops = true;
 #else
-    static constexpr bool vector_ops = false;
+  static constexpr bool vector_ops = false;
 #endif
 }
 #endif
