e4fb9e94e5a1219fa88c561966b76d1922774ba6
SPY Standalone Generator <spy@nowhere.com>
2026-05-30 17:11:20 +0000

    Latest SPY standalone

--- spy.hpp
+++ spy.hpp
@@ -1243,6 +1243,57 @@ namespace spy
   constexpr inline auto rvv_        = riscv_simd_info<_::simd_version::rvv_> {};
   constexpr inline auto fixed_rvv_  = riscv_simd_info<_::simd_version::fixed_rvv_> {};
 }
+#include <compare>
+#include <cstddef>
+namespace spy::_
+{
+  template<int Year> struct cpp_standard_t
+  {
+    static constexpr int                   year = Year;
+    template<int OtherYear> constexpr auto operator<=>(cpp_standard_t<OtherYear>) const
+    {
+      return year <=> OtherYear;
+    }
+    template<int OtherYear> constexpr bool operator==(cpp_standard_t<OtherYear>) const
+    {
+      return year == OtherYear;
+    }
+    constexpr auto operator<=>(int other_year) const { return year <=> other_year; }
+    constexpr bool operator==(int other_year) const { return year == other_year; }
+  };
+  template<_::stream OS, int Y> auto& operator<<(OS& os, cpp_standard_t<Y> const&)
+  {
+    if constexpr(Y != 0) return os << "C++" << Y;
+    else return os << "Unknown C++ standard version";
+  }
+}
+namespace spy
+{
+#if defined(_MSVC_LANG)
+#define SPY_RAW_CXX_STANDARD _MSVC_LANG
+#else
+#define SPY_RAW_CXX_STANDARD __cplusplus
+#endif
+#if SPY_RAW_CXX_STANDARD > 202302L
+#define SPY_CXX_VERSION 26
+#elif SPY_RAW_CXX_STANDARD >= 202302L
+#define SPY_CXX_VERSION 23
+#elif SPY_RAW_CXX_STANDARD >= 202002L
+#define SPY_CXX_VERSION 20
+#else
+#define SPY_CXX_VERSION 0
+#endif
+  inline constexpr _::cpp_standard_t<SPY_CXX_VERSION> cpp_standard {};
+}
+namespace spy::literal
+{
+  template<char... c>
+    requires(sizeof...(c) == 2)
+  constexpr auto operator""_cpp()
+  {
+    return _::cpp_standard_t<_::parse<c...>(0, 2)> {};
+  }
+}
 #include <cstddef>
 namespace spy::_
 {
