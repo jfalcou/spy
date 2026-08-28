1b771cb416c5cc69f564cc33557defa6fcc13814
Joel Falcou <joel.falcou@lri.fr>
2026-08-28 09:32:33 +0200

    Scope the warning flags to C++
    nvcc gets them verbatim otherwise, and its -Werror takes a value, so -Werror
    -Wall is fatal to it.

--- cmake/compiler.cmake
+++ cmake/compiler.cmake
@@ -13,14 +13,14 @@ target_compile_features ( spy_tests INTERFACE  cxx_std_20 )
 
 if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
   if(CMAKE_CXX_COMPILER_FRONTEND_VARIANT STREQUAL "MSVC")
-    target_compile_options( spy_tests INTERFACE /W3 /EHsc )
+    target_compile_options( spy_tests INTERFACE $<$<COMPILE_LANGUAGE:CXX>:/W3 /EHsc> )
   else()
-    target_compile_options( spy_tests INTERFACE -Werror -Wall -Wextra -Wunused-variable -Wshadow -Wdocumentation)
+    target_compile_options( spy_tests INTERFACE $<$<COMPILE_LANGUAGE:CXX>:-Werror -Wall -Wextra -Wunused-variable -Wshadow -Wdocumentation>)
   endif()
 elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-  target_compile_options( spy_tests INTERFACE /W3 /EHsc /Zc:preprocessor)
+  target_compile_options( spy_tests INTERFACE $<$<COMPILE_LANGUAGE:CXX>:/W3 /EHsc /Zc:preprocessor>)
 else()
-  target_compile_options( spy_tests INTERFACE -Werror -Wall -Wextra -Wunused-variable -Wshadow)
+  target_compile_options( spy_tests INTERFACE $<$<COMPILE_LANGUAGE:CXX>:-Werror -Wall -Wextra -Wunused-variable -Wshadow>)
 endif()
 
 target_include_directories( spy_tests INTERFACE
